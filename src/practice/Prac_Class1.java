5fa05d169ade704dbb0fa827c5cf1e8332cffc5a
Pooja Rajguru <poojarajguru3@gmail.com>
2024-12-14 10:45:26 +0530

    Branch A changes done

--- src/practice/Prac_Class1.java
+++ src/practice/Prac_Class1.java
@@ -3,6 +3,6 @@ package practice;
 public class Prac_Class1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello_class1");
+		System.out.println("Hello_class1 Branh A changes");
 	}
 }

06d3eba3743a912a130416a0e2ba2ff37082a8e3
Pooja Rajguru <poojarajguru3@gmail.com>
2024-12-14 10:36:33 +0530

    class 1

--- src/practice/Prac_Class1.java
+++ src/practice/Prac_Class1.java
@@ -2,4 +2,7 @@ package practice;
 
 public class Prac_Class1 {
 
+	public static void main(String[] args) {
+		System.out.println("Hello_class1");
+	}
 }
--- src/practice/Prac_Class2.java
+++ src/practice/Prac_Class2.java
@@ -1,5 +1,8 @@
 package practice;
 
 public class Prac_Class2 {
+	public static void main(String[] args) {
+		System.out.println("Hello_class2");
+	}
 
 }
